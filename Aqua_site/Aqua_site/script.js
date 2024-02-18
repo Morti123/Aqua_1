b939f33e8eb13b17eaf477bd4cb615351b2cd3b3
Morti123 <114169876+Morti123@users.noreply.github.com>
2024-02-18 19:46:05 +0300

    Add files via upload

---
+++ Aqua_site/Aqua_site/script.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper('.popular-slider', {
+    spaceBetween: 20,
+    pagination: {
+      el: '.swiper-pagination',
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
